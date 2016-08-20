ad8c2ac1ec69cf3ac05d80b0d8ec800126e614d8
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-20 15:05:48 -0700

    Blaming regex.

--- Writer.java
+++ Writer.java
@@ -1,3 +1,23 @@
 public class Writer{
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_BLACK = "\u001B[30m";
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_GREEN = "\u001B[32m";
+  public static final String ANSI_YELLOW = "\u001B[33m";
+  public static final String ANSI_BLUE = "\u001B[34m";
+  public static final String ANSI_PURPLE = "\u001B[35m";
+  public static final String ANSI_CYAN = "\u001B[36m";
+  public static final String ANSI_WHITE = "\u001B[37m";
+  public static final void write(String string, String color, boolean with_color) {
+    string += "\n";
+    try {
+      for(char c : string.toCharArray()) {
+        if(with_color) { System.out.print(color+c+ANSI_RESET); }
+        else { System.out.print(c); }
+        Thread.sleep(5);
+      }
+    }
+    catch(InterruptedException e) { }
+  }
   public static final void say(Object obj) { write(obj.toString(),"",false); }
 }
