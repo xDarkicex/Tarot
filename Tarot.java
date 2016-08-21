a04053d77faca2360e84e74302fd46dbd70bbc46
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-20 17:39:26 -0700

    Best commit ever

--- Tarot.java
+++ Tarot.java
@@ -2,12 +2,22 @@ import java.io.Console;
 
 public class Tarot{
   private Console console = System.console();
-  String input = console.readLine();
-  public void run(){
-  Writer.say(input);
+  public void init(){
+
+      Writer.say("What is your name?");
+      String name = console.readLine();
+      Writer.say("What is your age?");
+      String age = console.readLine();
+      Wrtier.say("What is your Gender");
+      String gender = console.readLine();
+  public static String information(){
+    Writer.say("Name: ["+name+"]");
+    Writer.say("Say confirm options?");
+
+  };
   //why doesnt this work?
 };
 
   private static Tarot reading = new Tarot();
-  public static void main(String args[]){ reading.run(); }
+  public static void main(String args[]){ reading.init(); }
 };
